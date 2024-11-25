75ac64e0d71adb6965852268be083b7bbeb9a63f
Saynez667 <saynezironx@gmail.com>
2024-11-25 16:16:04 +0100

    on a commence a ecire du code pour le printf.c on a fais une boucle qui verife si format n'est pas egal %

---
+++ printf.c
@@ -0,0 +1,21 @@
+#include "main.h"
+int _printf(const char *format, ...)
+{
+    va_list args;
+    int printed_chars = 0;
+
+    va_start(args, format);
+while (*format)
+{
+if(*format != '%')
+}    
+write(1,format, 1);
+
+
+
+// Parcourir la chaîne de format
+    // Gérer les différents cas : %c, %s, %%
+
+    va_end(args);
+    return (printed_chars);
+}
