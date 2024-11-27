0f630657d9e964a97ea1d39555f6bee39aca50d2
root <saynezironx@gmail.com>
2024-11-27 17:13:29 +0100

    betty coding style

--- byte.c
+++ byte.c
@@ -1,4 +1,16 @@
 #include "main.h"
+/*
+*Fichier: fonctions_impression.c
+*Description: Ce fichier contient des fonctions utilitaires pour l'impression
+*de différents types de données (caractère, chaîne, pourcentage) dans le cadre
+*d'une implémentation personnalisée de printf ou d'une fonction similaire.
+*Ces fonctions sont conçues pour être utilisées avec une liste d'arguments
+*variable (va_list) et retournent le nombre de caractères imprimés.
+* onctions incluses:
+*- print_char: Imprime un seul caractère
+*- print_string: Imprime une chaîne de caractères
+*- print_percent: Imprime le caractère '%' 
+*/
 int print_char(va_list args)
 {
 char c = va_arg(args, int);
